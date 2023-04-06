2a3e8df2bc3a1a9991ce0e85be995d34072b589b
Dyzfunkshin <mcampbell3702@gmail.com>
2023-04-05 21:39:19 -0400

    Added a script to copy build output to the mod manager folder.

---
+++ copy_dll.bat
@@ -0,0 +1,8 @@
+@echo off
+
+setlocal
+
+echo TODO: Update copy_dll.bat to properly copy build output to the game directory!
+REM xcopy /Y "C:\Users\mcamp\OneDrive\Documents\GitHub\DyzzysDyzfunkshinalCardsPlugin\Temp\bin\Debug\DyzzysDyzfunkshinalCardsPlugin.dll" "C:\Users\mcamp\AppData\Roaming\Thunderstore Mod Manager\DataFolder\ROUNDS\profiles\Debugging\BepInEx\plugins\Unknown-DyzzysDyzfunkshinalCardsPlugin"
+
+endlocal
